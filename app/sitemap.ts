403ba6e3d5cb60512627011dc4550b92274f7dd1
Toshpolat-gif <toshpolatnazarov@gmail.com>
2026-09-24 08:12:40 +0500

    Update sitemap revalidation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 import { getPublishedArticles, getPublishedConferences } from "@/lib/public-data";
 import { siteConfig } from "@/lib/site";
 
+export const revalidate = 60;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const [conferences, articles] = await Promise.all([
     getPublishedConferences(500).catch(() => []),
